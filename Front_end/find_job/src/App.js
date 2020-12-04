3ba4cfd83a6b3819fca938dc1b17eac336735fba
quochuy3401 <huyapolo34@gmail.com>
2020-12-04 19:30:58 +0700

    huy 19h30

--- Front_end/find_job/src/App.js
+++ Front_end/find_job/src/App.js
@@ -1,28 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
-import { Route } from "react-router-dom";
-import { NavDropdown, Nav, Button, Navbar } from "react-bootstrap";
+import { Route, Link, useHistory } from "react-router-dom";
+import { NavDropdown, Nav, Button, Navbar, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Companies } from "./components/Company/Companies";
+import { Home } from "./components/Home/index";
+
+
+
 
 export const App = () => {
+  const history = useHistory()
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home" className="mx-2">
-              Jobs
-            </Nav.Link>
-            <Nav.Link href="#link">Companies</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Route component={Jobs} />
+      
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/jobs" className="mx-1">
+                Jobs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/companies" className="mx-1">Companies</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      
+      {/* <Route component={Jobs} />
       <Route component={Companies} />
-      <Route component={Auth} />
+      <Route component={Auth} /> */}
+      <>
+        <Route  exact path="/" component={Home} />
+        <Route exact path="/companies" component={Companies} />
+      </>
     </div>
   );
 };
