6a9b1848534aa00d782208499185a38960a8235b
Manfred11 <manhcuong.qn1998@gmail.com>
2020-12-15 16:31:54 +0700

    update css , app.js

--- Front_end/employers/src/App.js
+++ Front_end/employers/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import {
   Nav,
@@ -11,6 +10,7 @@ import {
   NavDropdown,
 } from "react-bootstrap";
 import { Route, useHistory, Link } from "react-router-dom";
+import "../src/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { faArrowRight, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -121,6 +121,61 @@ function App() {
           </>
         )}
       </authContext.Provider>
+
+      <div id="footer" className="p-5">
+          <Row>
+            <div className="d-flex" style={{ height: "max-content" }}>
+              <Col xs={4} className="aside-left">
+                <img src={"/image/company-1.png"} />
+                <p className="para-left">
+                  Find Jobs is the #1 recruitment platform in Asia helping
+                  companies build successful teams with young talent. Our
+                  mission is to help companies hire the right young talent
+                  effectively, and for young people to discover and develop
+                  careers they love.
+                </p>
+                <p>
+                  © 2020 Find Jobs Intern Pte Ltd & Find Jobs Vietnam Pte Ltd
+                  <br></br>
+                  EA Licence No: 16C7981
+                </p>
+              </Col>
+
+              <Col className="ml-auto" xs={6}>
+                {/* <div className="mx-auto" style={{color:"white"}}>
+                  <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                  <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                </div> */}
+                <div className="d-flex">
+                  <div className=" each-col px-0">
+                    <div className="title-right">Company</div>
+                    <div className="detail-right">About us</div>
+                    <div className="detail-right">Hired Blog</div>
+                    <div className="detail-right">Inside FInd Jobs</div>
+                    <div className="detail-right">Careeers</div>
+                    <div className="detail-right">Terms&Conditions</div>
+                  </div>
+                  <div className="each-col px-0">
+                    <div className="title-right">FOR JOB SEEKERS</div>
+                    <div className="detail-right">Jobs by Location</div>
+                    <div className="detail-right">Jobs by Company Name</div>
+                    <div className="detail-right">Jobs by Category</div>
+                  </div>
+                  <div className="each-col px-0">
+                    <div className="title-right">BUSINESS SOLUTIONS</div>
+                    <div className="detail-right">For Employers</div>
+                    <div className="detail-right">Find Jobs Platform</div>
+                    <div className="detail-right">TalentHunt</div>
+                    <div className="detail-right">TalentHub</div>
+                  </div>
+                </div>
+              </Col>
+            </div>
+          </Row>
+        </div>
     </div>
   );
 }
